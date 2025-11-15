56cee0301f6b755988c2aff739f7f9c778793518
Francisco <88842468+franciscofrask@users.noreply.github.com>
2025-11-15 18:29:09 -0300

    Delete TPHerencias directory

--- TPHerencias/src/main/java/kata1_vehiculos/MainKata1.java
+++
@@ -1,8 +0,0 @@
-package main.java.kata1_vehiculos;
-
-public class MainKata1 {
-    public static void main(String[] args) {
-        Auto a = new Auto("Toyota", "Corolla", 4);
-        a.mostrarInfo();
-    }
-}
